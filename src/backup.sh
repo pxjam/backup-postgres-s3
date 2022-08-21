6b4ad442b4534ded92f4edd7861816d70193ab3d
d.udovichenko <d.udovichenko@gmail.com>
2022-08-22 00:03:18 +0300

    feat: more explicit env var names

--- src/backup.sh
+++ src/backup.sh
@@ -5,13 +5,13 @@ echo "CRON MAKES BACKUP OF $BAKPGS3_PROJECT_NAME AT $(date)"
 rm -f /root/*.sql.gz
 
 DUMP_FILENAME="/root/${BAKPGS3_PROJECT_NAME}.$(date '+%F').sql.gz"
-export PGPASSWORD="$BAKPGS3_PASSWORD"
+export PGPASSWORD="$BAKPGS3_DB_PASSWORD"
 
 pg_dump --format=custom \
-	-h "$BAKPGS3_HOST" \
+	-h "$BAKPGS3_DB_HOST" \
 	-p 5432 \
-	-U "$BAKPGS3_USER" \
-	-d "$BAKPGS3_DATABASE" |
+	-U "$BAKPGS3_DB_USER" \
+	-d "$BAKPGS3_DB_DATABASE" |
 	gzip >"$DUMP_FILENAME"
 
-s3cmd put "$DUMP_FILENAME" "s3://$BAKPGS3_BUCKET"
+s3cmd put "$DUMP_FILENAME" "s3://$BAKPGS3_S3_BUCKET"
